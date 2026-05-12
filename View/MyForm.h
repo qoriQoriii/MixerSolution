18484253c71d19115c0b23144acd44a603832d7c
alulab14 <alulab14@inf.pucp.edu.pe>
2026-05-11 19:32:22 -0500

    Cambio de foto

--- View/MyForm.h
+++ View/MyForm.h
@@ -21,9 +21,8 @@ namespace View {
 		MyForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
+			this->BackgroundImage = Image::FromFile(Application::StartupPath + "\\bar.png");
+			this->BackgroundImageLayout = ImageLayout::Stretch;
 		}
 
 	protected:
@@ -46,8 +45,9 @@ namespace View {
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
+
+
 
 	private:
 		/// <summary>
@@ -62,20 +62,16 @@ namespace View {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->txtUsuario = (gcnew System::Windows::Forms::TextBox());
 			this->txtContra = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// txtUsuario
 			// 
-			this->txtUsuario->Location = System::Drawing::Point(233, 157);
+			this->txtUsuario->Location = System::Drawing::Point(179, 202);
 			this->txtUsuario->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->txtUsuario->Name = L"txtUsuario";
 			this->txtUsuario->Size = System::Drawing::Size(148, 26);
@@ -84,7 +80,7 @@ namespace View {
 			// 
 			// txtContra
 			// 
-			this->txtContra->Location = System::Drawing::Point(233, 288);
+			this->txtContra->Location = System::Drawing::Point(179, 274);
 			this->txtContra->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->txtContra->Name = L"txtContra";
 			this->txtContra->Size = System::Drawing::Size(148, 26);
@@ -94,71 +90,55 @@ namespace View {
 			// label1
 			// 
 			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::Transparent;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->label1->Location = System::Drawing::Point(64, 157);
+			this->label1->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			this->label1->Location = System::Drawing::Point(35, 202);
 			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(43, 20);
 			this->label1->TabIndex = 2;
 			this->label1->Text = L"User";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::Color::Transparent;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(64, 288);
+			this->label2->ForeColor = System::Drawing::Color::Transparent;
+			this->label2->Location = System::Drawing::Point(35, 274);
 			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(78, 20);
 			this->label2->TabIndex = 3;
 			this->label2->Text = L"Password";
+			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
 			// 
 			// button1
 			// 
-			this->button1->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->button1->BackColor = System::Drawing::SystemColors::GradientInactiveCaption;
+			this->button1->FlatAppearance->BorderSize = 0;
 			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
 			this->button1->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->button1->Location = System::Drawing::Point(598, 211);
+			this->button1->Location = System::Drawing::Point(127, 363);
 			this->button1->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(112, 35);
 			this->button1->TabIndex = 4;
 			this->button1->Text = L"Ingresar";
+			this->button1->UseCompatibleTextRendering = true;
 			this->button1->UseVisualStyleBackColor = false;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
-			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(327, 52);
-			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(116, 31);
-			this->label3->TabIndex = 5;
-			this->label3->Text = L"Login v1";
-			// 
-			// pictureBox1
-			// 
-			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(36, 333);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(170, 271);
-			this->pictureBox1->TabIndex = 6;
-			this->pictureBox1->TabStop = false;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ControlDarkDark;
-			this->ClientSize = System::Drawing::Size(798, 629);
-			this->Controls->Add(this->pictureBox1);
-			this->Controls->Add(this->label3);
+			this->ClientSize = System::Drawing::Size(865, 579);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
@@ -171,7 +151,6 @@ namespace View {
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -211,5 +190,11 @@ namespace View {
 			txtUsuario->Focus();
 		}
 	}
+private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
 };
 }
